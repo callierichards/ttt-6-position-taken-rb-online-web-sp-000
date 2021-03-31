b3328d344492f996f65c4c56aad34fc828f76e09
callie rogers <callie1013@gmail.com>
2021-03-31 16:42:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,11 @@
-def position_taken(board, index)
-  board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  index = board.i-1
+def position_taken?(board, index="X")
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  index = board.to_i - 1
+if board[index] == "" || board[index] == " " || board[index] == nil
+  return false
+  puts "Pick another space"
+else
+  return true
+  puts "Good choice"
+end
 end
